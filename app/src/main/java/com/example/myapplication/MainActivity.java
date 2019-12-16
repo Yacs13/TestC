21ed568569377d771497f7e6830eb84414ee9a23
Yuser Arthuro Cueva Sánchez <ycuevasa@everis.com>
2019-12-16 14:01:48 -0500

    Android project

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        String cadena = "Hola mundo";
+
+        Integer contador = 10;
+
+        contador += 2;
+
+        
+    }
+}
